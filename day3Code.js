1a0584254f323f07d2d3370339fe895a4bdaa4dd
darryldriedger <darryldriedger@gmail.com>
2022-12-10 14:15:37 -0700

    Day 3 part 2 complete

--- day3Code.js
+++ day3Code.js
@@ -7,21 +7,24 @@ function syncReadFile(filename) {
     const arr = contents.split(/\r?\n/);
     let arrCopy = [...arr]
 
-    for(let i = 0; i < arrCopy.length; i++){ //arrCopy.length
+    for(let i = 0; i < arrCopy.length; i += 3){
         let letterSet = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        let pocketStash = arrCopy[i].length / 2
-        let pocket1 = arrCopy[i].slice(0,pocketStash)
-        let pocket2 = arrCopy[i].slice(pocketStash,arrCopy[i].length)
-        let pattern = new RegExp(`[${pocket1}]`, "g")
-        let priorities = [...new Set(pocket2.match(pattern))]
-        for(let p = 0; p < priorities.length; p++){
-            let letterVal = letterSet.indexOf(`${priorities[p]}`)
+        let pack1 = arrCopy[i]
+        let pack2 = arrCopy[i + 1]
+        let pack3 = arrCopy[i + 2]
+        let pattern1 = new RegExp(`[${pack1}]`, "g")
+        let pattern3 = new RegExp(`[${pack3}]`, "g")
+        let priorities = [...new Set(pack2.match(pattern1))]
+        let packCheckOneTwo = priorities.join("")
+        let priorities2 = [...new Set(packCheckOneTwo.match(pattern3))]
+        for(let p = 0; p < priorities2.length; p++){
+            let letterVal = letterSet.indexOf(`${priorities2[p]}`)
             priorityArr.push(letterVal)
         }
     }
+    console.log(priorityArr)
     console.log(priorityArr.reduce((accumulator, currentValue) => accumulator + currentValue,0))
     return priorityArr
 }
-
 
 syncReadFile('./day3Data.txt');
