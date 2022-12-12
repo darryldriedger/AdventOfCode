ac52edb93777f9690f7f3dae8ab27afa31d98146
darryldriedger <darryldriedger@gmail.com>
2022-12-12 00:09:41 -0700

    Day 4 part 1 complete

---
+++ day4code.js
@@ -0,0 +1,32 @@
+const { Console } = require('console');
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
+    const arr = contents.split(/\r?\n/);
+    let arrCopy = [...arr]
+
+    let Count = 0
+    let row = []
+    for(let i = 0; i < arrCopy.length; i++){
+        let teams = arrCopy[i]
+        let step1 = teams.split(",")
+
+        row = []
+        for(let j = 0; j < step1.length; j++){
+            const start = Number(step1[j].split('-')[0])
+            const finish = Number(step1[j].split('-')[1])
+            row.push(start)
+            row.push(finish)
+        } 
+        if(row[0] >= row[2] && row[1] <= row[3]){
+            Count ++
+        } else if(row[2] >= row[0] && row[3] <= row[1]){
+            Count ++
+        }
+    }
+    console.log(Count)
+    return Count
+}
+
+syncReadFile('./day4Data.txt')
