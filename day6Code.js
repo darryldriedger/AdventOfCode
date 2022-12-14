a4b063a3db6f5be81179c7da02e8595f6a4c1b56
darryldriedger <darryldriedger@gmail.com>
2022-12-14 15:35:53 -0700

    Day 6 part 2 complete

--- day6Code.js
+++ day6Code.js
@@ -5,12 +5,13 @@ function syncReadFile(filename) {
     const contents = readFileSync(filename, 'utf-8');
     const arr = contents.split(/\r?\n/);
     let text = arr[0]
+    let packetLength = 14
     for(i = 0; i < text.length; i++){//text.length
-        let extraction = text.slice(i,i+4)
+        let extraction = text.slice(i,i + packetLength)
         let test = extraction.match(/^.*(.).*\1.*$/)
         if(test === null){
-            console.log(text.indexOf(extraction) +4)
-            console.log(i +4)
+            console.log(text.indexOf(extraction) + packetLength)
+            console.log(i + packetLength)
             return extraction
         }
         }
