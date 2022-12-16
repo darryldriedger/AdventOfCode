21ba2b43516ede906a1839f9d5ed9c0a5d511d22
darryldriedger <darryldriedger@gmail.com>
2022-12-16 05:30:06 -0700

    working on Day 7 part1

--- day7Code.js
+++ day7Code.js
@@ -7,60 +7,69 @@ function syncReadFile(filename) {
     const arr = contents.split(/\r?\n/);
     // console.log(arr)
 
-    let filesys = { root: {} }
-    // filesys.root = {}    
-    let position = filesys.root
+    let filesys = {}
+    let position 
     let prevPosition
     let cmdCounter = 0
 
-    // class filesys {
-    //     constructor(value) {
-    //       this.value = value;
-    //       this.type = "";
-    //       this.descendants = [];
-    //       this.parent = null;
-    //       this.fileSize = null;
-    //     }
-    // }
-    // let root = new filesys('root');
-
-    for(cmdCounter; cmdCounter < 30; cmdCounter++){              //arr.length
+    class sysNode {
+        constructor(value) {
+          this.value = value;
+          this.type = null;
+          this.descendants = {};
+          this.parent = null;
+          this.position = null;
+          this.fileSize = null;
+        }
+    }
+    let root = new sysNode('root')
+    filesys.root = root
+    position = filesys.root
+    console.log(filesys)
+    
+    for(cmdCounter; cmdCounter < arr.length; cmdCounter++){              //arr.length
         termLine = arr[cmdCounter].split(" ")
-        // if(termLine.length === 2){
-        //     console.log(termLine)
-        // } else {}
-        // console.log(position)
-            console.log(termLine)
-
+        console.log(termLine)
         if(termLine[0] === "$"){
             if(termLine[1] === "cd"){
                 if(termLine.includes("/")){
-                    // console.log("go to root")
                     position = filesys.root
-                    // console.log(position)
-                } else if(termLine[3] === ".."){
-                    // console.log("go to parent")
+                } else if(termLine.includes("..")){
+                    console.log("test")
+                    console.log(position)
+                    // position = position.parent
                 } else if(termLine[3] !== ""){
+
                     // console.log("navigate from current position")
+                    // console.log(termLine)
+                    // console.log(position)
+                    // newObject = termLine[3]
+                    // position[newObject] = {}
+                    // console.log(filesys.root)
+                    // position.descendants = new sysNode(termLine[3])
                 }
             } else if(termLine[1] = "ls"){
                 // let list = []
                 // cmdCounter++
             //    console.log("list out the files")
             // console.log(termLine)
-            do{
-                cmdCounter++
-                termLine = arr[cmdCounter].split(" ")
-                console.log(`==> ${termLine}`)
-                // cmdCounter++
-            }while(termLine[0] !== "$")
+
+
+            // let newObj = new sysNode(termLine[1])
+            //         position.descendants.push(newObj)
+            // do{
+            //     cmdCounter++
+            //     termLine = arr[cmdCounter].split(" ")
+            //     // console.log(`==> ${termLine}`)
+            //     // cmdCounter++
+            // }while(termLine[0] !== "$")
 
             //    list = arr[cmdCounter]
             //    list = list.split(" ")
             //    console.log(list)
             //    position.descendants = []
             //    position.descendants.push(list);
-            position
+            // position
             //    console.log(filesys)
             //    console.log("files listed")
             //    console.log(filesys.root)
