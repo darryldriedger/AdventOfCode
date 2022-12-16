910b353bfcf7e778ab29aac4314609fc04da627b
darryldriedger <darryldriedger@gmail.com>
2022-12-15 20:43:47 -0700

    Day 7 working on part 1

--- day7Code.js
+++ day7Code.js
@@ -1,4 +1,3 @@
-//https://adrianmejia.com/data-structures-for-beginners-trees-binary-search-tree-tutorial/
 
 const { Console } = require('console');
 const {readFileSync, promises: fsPromises} = require('fs');
@@ -25,31 +24,44 @@ function syncReadFile(filename) {
     // }
     // let root = new filesys('root');
 
-    for(cmdCounter; cmdCounter < arr.length; cmdCounter++){              //arr.length
+    for(cmdCounter; cmdCounter < 30; cmdCounter++){              //arr.length
         termLine = arr[cmdCounter].split(" ")
-        // console.log(termLine)
+        // if(termLine.length === 2){
+        //     console.log(termLine)
+        // } else {}
         // console.log(position)
+            console.log(termLine)
+
         if(termLine[0] === "$"){
             if(termLine[1] === "cd"){
                 if(termLine.includes("/")){
                     // console.log("go to root")
                     position = filesys.root
-                    console.log(position)
+                    // console.log(position)
                 } else if(termLine[3] === ".."){
                     // console.log("go to parent")
                 } else if(termLine[3] !== ""){
                     // console.log("navigate from current position")
                 }
-            } else if(termLine[0] = "ls"){
-                let list = []
-                cmdCounter++
+            } else if(termLine[1] = "ls"){
+                // let list = []
+                // cmdCounter++
             //    console.log("list out the files")
-               list = arr[cmdCounter]
-               list = list.split(" ")
-               console.log(list)
-               position.descendants = []
-               position.descendants.push(list);
-            //    console.log(position.descendants)
+            // console.log(termLine)
+            do{
+                cmdCounter++
+                termLine = arr[cmdCounter].split(" ")
+                console.log(`==> ${termLine}`)
+                // cmdCounter++
+            }while(termLine[0] !== "$")
+
+            //    list = arr[cmdCounter]
+            //    list = list.split(" ")
+            //    console.log(list)
+            //    position.descendants = []
+            //    position.descendants.push(list);
+            position
+            //    console.log(filesys)
             //    console.log("files listed")
             //    console.log(filesys.root)
 
