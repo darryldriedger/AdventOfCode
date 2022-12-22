5f5bfe1be0c9959bb437478cecebda40e904a9df
darryldriedger <darryldriedger@gmail.com>
2022-12-21 23:15:01 -0700

    working on day 7 part 1

--- day7Code.js
+++ day7Code.js
@@ -14,6 +14,7 @@ function syncReadFile(filename) {
         constructor(value) {
           this.value = value;
           this.type = null;
+          this.name = "";
           this.descendants = [];
           this.parents = [];
           this.directParent = null;
@@ -44,12 +45,23 @@ function syncReadFile(filename) {
             } else if(termLine[1] = "ls"){
                 cmdCounter++ 
                 termLine = arr[cmdCounter].split(" ")
+                let newName = termLine[1]
+
+                // if(filesys[termLine[1]]){
+                //     let i = 0
+                //     do {
+                //         newName = `${newName}_${i}`
+                //         i++;
+                //       }
+                //       while (filesys[newName])
+                // } else {newName}
 
                 for (cmdCounter; cmdCounter < arr.length; cmdCounter++) {
                     termLine = arr[cmdCounter].split(" ")
                     if (termLine.includes("$")) { break; }
 
-                    let newNode = new sysNode(termLine[1])
+                    
+                    let newNode = new sysNode(newName)
                     filesys[newNode.value] = newNode
 
                     if(termLine.includes("dir")){
@@ -58,9 +70,11 @@ function syncReadFile(filename) {
                         newNode.type = "file"
                         newNode.fileSize = Number(termLine[0])
                     }
+                        newNode.name = termLine[1]
                         newNode.parents.push(position)
                         newNode.directParent = filesys[position].value
                         filesys[position].descendants.push(newNode.value)
+                        console.log(`${newNode.value} ${newNode.type} ${newNode.fileSize} ${newNode.directParent}`)
                   }
                   cmdCounter--
             }
@@ -76,36 +90,35 @@ function syncReadFile(filename) {
     //         }
     //      }
     // })
-        Object.keys(filesys).reverse().forEach((key) => {
-        if (filesys[key].type === 'dir') {  //key !== 'root' && 
-            let childList = filesys[key].descendants
-            for(i = 0; i < childList.length; i++){
-                let child = childList[i]
-                let childSize = filesys[child].fileSize
-                filesys[key].fileSize += childSize
-            }
-         }
+
+    Object.keys(filesys).reverse().forEach((key) => {
+                let parent = filesys[key].directParent
+                if(parent !== null){
+                    filesys[parent].fileSize += filesys[key].fileSize
+                } else {
+                    console.log(filesys[parent])
+                }
     })
 
-    //   let dirTotal = 0
-    // Object.keys(filesys).forEach((key) => {
-    //     if(filesys[key].type === "dir"){
-    //         if(filesys[key].fileSize <= 100000){
-    //             dirTotal += filesys[key].fileSize
-    //         }
-    //         if(filesys[key].fileSize === 0){
-    //             console.log(filesys[key].value)
-    //         }
-    //     }
-    //     return dirTotal
-    // })
+      let dirTotal = 0
+    Object.keys(filesys).forEach((key) => {
+        if(filesys[key].type === "dir"){
+            if(filesys[key].fileSize <= 100000){
+                dirTotal += filesys[key].fileSize
+            }
+            if(filesys[key].fileSize === 0){
+                // console.log(filesys[key].value)
+            }
+        }
+        return dirTotal
+    })
     // console.log(filesys)
-    // console.log(`The directory Total under 100000 is ${dirTotal}`)
+    console.log(`The directory Total under 100000 is ${dirTotal}`)
     // console.log(filesys['jfp'])
     // console.log(filesys['wvdqjn'])
-    // console.log(filesys['zmllsrzc'])
-    // console.log(filesys['slwhsqw'])
-    // console.log(filesys['bgv'])
+    // console.log(filesys['mgqdbgm'])
+    // console.log(filesys['fhnz'])
+    // console.log(filesys['scdlp'])
     // console.log(filesys['bvqwrs'])
     //1723892
         return filesys
